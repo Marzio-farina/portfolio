f8870709ab95463a1c719f04626c7c6ab850f28b
Marzio <marziofarina@icloud.com>
2025-10-05 19:28:25 +0200

    aggiunto il routing per le varie rotte tra le pagine

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,15 @@
+// app.routes.ts
 import { Routes } from '@angular/router';
-import { App } from './app';
 import { About } from './pages/about/about';
 import { Contatti } from './pages/contatti/contatti';
 import { Curriculum } from './pages/curriculum/curriculum';
 import { Progetti } from './pages/progetti/progetti';
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: App,
-        children: [
-            { path: '', pathMatch: 'full', redirectTo: 'about' },
-            { path: 'about', component: About },
-            { path: 'about', component: Contatti },
-            { path: 'about', component: Curriculum },
-            { path: 'about', component: Progetti },
-        ],
-    },
-    { path: '**', redirectTo: '' },
+  { path: '', pathMatch: 'full', redirectTo: 'about' },
+  { path: 'about', component: About },
+  { path: 'contatti', component: Contatti },
+  { path: 'curriculum', component: Curriculum },
+  { path: 'progetti', component: Progetti },
+  { path: '**', redirectTo: 'about' },
 ];
