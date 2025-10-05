c7e083d8404f21f1a868feade0619720393213db
Marzio <marziofarina@icloud.com>
2025-10-05 19:13:10 +0200

    aggiunte le varie rotte alle varie pagine

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { App } from './app';
+import { About } from './pages/about/about';
+import { Contatti } from './pages/contatti/contatti';
+import { Curriculum } from './pages/curriculum/curriculum';
+import { Progetti } from './pages/progetti/progetti';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: App,
+        children: [
+            { path: '', pathMatch: 'full', redirectTo: 'about' },
+            { path: 'about', component: About },
+            { path: 'about', component: Contatti },
+            { path: 'about', component: Curriculum },
+            { path: 'about', component: Progetti },
+        ],
+    },
+    { path: '**', redirectTo: '' },
+];
