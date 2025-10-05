7c3370d454f6ecb577a33fea24ba2f5af511e8dc
Marzio <marziofarina@icloud.com>
2025-10-05 15:13:50 +0200

    rimosso tutto il css ed aggiunto il componente dashboard

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { Aside } from "./components/aside/aside";
 import { Navbar } from "./components/navbar/navbar";
+import { Dashboard } from "./components/dashboard/dashboard";
 
 @Component({
   selector: 'app-root',
-  imports: [Aside, Navbar],
+  imports: [Aside, Navbar, Dashboard],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
