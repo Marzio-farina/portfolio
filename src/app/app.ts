1245f43b5fe8b9d89396a97f2b8d6162c0c9078d
Marzio <marziofarina@icloud.com>
2025-10-05 13:03:47 +0200

    creazione del componente aside

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Aside } from "./components/aside/aside";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Aside],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
