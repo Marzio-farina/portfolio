b4b3f327d68dbb2af7a20a51e7ea46296a1d0e5f
Marzio <marziofarina@icloud.com>
2025-10-06 17:57:19 +0200

    modificato la configurazione di tailwind per il tema giorno/notte e tutto il css in modo dinamico

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
+const themes = require('daisyui/src/theming/themes');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,ts}'],
-  theme: { extend: {} },
+  darkMode: 'media',
+  theme: { 
+    extend: {}
+  },
   plugins: [require('daisyui')],
   daisyui: {
-    themes: ['light', 'business'],
-    darkTheme: 'business',
+    themes: false,
   },
 };
