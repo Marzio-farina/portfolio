6b9b3801a25146ec0f46630096016c4571903f40
Marzio <marziofarina@icloud.com>
2025-10-09 15:47:16 +0200

    creato il componente card

--- src/app/pages/about/about.ts
+++ src/app/pages/about/about.ts
@@ -2,10 +2,11 @@ import { Component, inject } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { ActivatedRoute } from '@angular/router';
 import { map } from 'rxjs';
+import { Card } from '../../components/card/card';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [Card],
   templateUrl: './about.html',
   styleUrl: './about.css'
 })
