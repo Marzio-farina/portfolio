0bfa4b5f37e4c122e8d5cc8577039ba9e4d90e71
Marzio <marziofarina@icloud.com>
2025-10-09 17:15:59 +0200

    reso dinamica la generazione delle card della pagina about in base al json

--- src/app/pages/about/about.ts
+++ src/app/pages/about/about.ts
@@ -1,8 +1,15 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { ActivatedRoute } from '@angular/router';
 import { map } from 'rxjs';
 import { Card } from '../../components/card/card';
+import { HttpClient } from '@angular/common/http';
+
+type AboutCard = {
+  id: string;
+  title: string;
+  description: string;
+};
 
 @Component({
   selector: 'app-about',
@@ -12,5 +19,26 @@ import { Card } from '../../components/card/card';
 })
 export class About {
   private route = inject(ActivatedRoute);
+  private http = inject(HttpClient);
+
   title = toSignal(this.route.data.pipe(map(d => d['title'] as string)), { initialValue: '' });
+
+  cards = signal<AboutCard[]>([]);
+  loading = signal(true);
+  errorMsg = signal<string | null>(null);
+
+  constructor() {
+    this.http.get<AboutCard[]>('assets/about/cards.json').subscribe({
+      next: data => {
+        this.cards.set(data ?? []);
+        this.loading.set(false);
+      },
+      error: () => {
+        this.errorMsg.set('Impossibile caricare le card.');
+        this.loading.set(false);
+      }
+    });
+  }
+
+  trackById = (_: number, c: AboutCard) => c.id;
 }
