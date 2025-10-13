3efe8ebabd4b5ff50bb48d08cc3fd96826706eee
Marzio <marziofarina@icloud.com>
2025-10-13 12:42:13 +0200

    modifiche a progetti.json e tipizzato con type progetto

--- src/app/components/progetti-card/progetti-card.ts
+++ src/app/components/progetti-card/progetti-card.ts
@@ -1,5 +1,15 @@
 import { Component, input } from '@angular/core';
 
+export type Progetto = {
+  id: number;
+  title: string;
+  category: string;
+  description: string;
+  poster: string; // data URI o url immagine
+  video: string;  // url mp4/webm locale
+  technologies : string;
+};
+
 @Component({
   selector: 'app-progetti-card',
   imports: [],
@@ -7,5 +17,5 @@ import { Component, input } from '@angular/core';
   styleUrl: './progetti-card.css'
 })
 export class ProgettiCard {
-  progetto = input.required<{ title: string; category: string; video: string }>();
+  progetto = input.required<Progetto>();
 }
