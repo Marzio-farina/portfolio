fcec11afe497f4e23cfa10dca6a56ffe830d2394
Marzio <marziofarina@icloud.com>
2025-10-18 18:52:12 +0200

    tabella CV

---
+++ backend/database/migrations/2025_10_18_165132_c_v.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('curricula', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title', 50);
+            $table->date('time_start');
+            $table->date('time_end')->nullable();
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('curricula');
+    }
+};
