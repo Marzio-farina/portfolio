a1a0b4edf2a060110ef5c5838e74a9225d6545f5
Marzio <marziofarina@icloud.com>
2025-10-18 18:50:16 +0200

    tabella projects

---
+++ backend/database/migrations/2025_10_18_164950_projects.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title', 50);
+            $table->unsignedSmallInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories')->cascadeOnDelete();
+
+            $table->text('description');
+            $table->string('poster', 255)->nullable();
+            $table->string('video', 255)->nullable();
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
