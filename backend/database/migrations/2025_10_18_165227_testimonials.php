796e8d7b9b67f0895ae13488195326441e9bb90d
Marzio <marziofarina@icloud.com>
2025-10-18 18:53:45 +0200

    tabella testimonials

---
+++ backend/database/migrations/2025_10_18_165227_testimonials.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('testimonials', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
+
+            $table->text('text');
+            $table->string('role_company', 150)->nullable();
+            $table->string('company', 150)->nullable();
+            $table->smallInteger('rating'); // 1..5
+
+            $table->timestamps();
+        });
+
+        // CHECK constraint per Postgres
+        DB::statement('ALTER TABLE testimonials ADD CONSTRAINT testimonials_rating_check CHECK (rating BETWEEN 1 AND 5)');
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('testimonials');
+    }
+};
