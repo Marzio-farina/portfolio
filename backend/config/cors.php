e57202b298f3967775c42e40cd3a0bd8f26b8045
Marzio <marziofarina@icloud.com>
2025-10-18 11:04:48 +0200

    modifiche 2 conf

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -6,6 +6,8 @@ return [
     'allowed_origins' => [
         'https://marziofarina.it',
         'https://www.marziofarina.it',
+        'https://api.marziofarina.it',
+        'http://localhost:4200'
     ],
     'allowed_origins_patterns' => [
         '/^https:\/\/.*\.vercel\.app$/'
