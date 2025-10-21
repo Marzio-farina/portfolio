0c80af28690bea4438a9a7df2cd5fac0beb9f2eb
Marzio <marziofarina@icloud.com>
2025-10-21 09:54:51 +0200

    modifiche a bootstrap/app.php

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -12,6 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        apiPrefix: env('API_PREFIX', 'api'),
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->use([
